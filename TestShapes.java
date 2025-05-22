0bb2b32d52f7f3eb491f16bc5206762c590b79cd
ANANDANATARAJANAND <146619604+ANANDANATARAJANAND@users.noreply.github.com>
2025-05-22 13:15:53 +0530

    Delete TestShapes.java

--- TestShapes.java
+++
@@ -1,9 +0,0 @@
-public class TestShapes {
-    public static void main(String[] args) {
-        OBJECT1 s1 = new Circle();  
-        OBJECT1 s2 = new Square();
-
-        s1.draw();  
-        s2.draw();  
-    }
-}
